e74c067117e7ff01bae014f41efadad5bbcb67c7
Sasan <sasanfarhadi@gmail.com>
2024-12-09 00:38:48 +0330

    banner handle

--- Client/src/components/module/Banner.js
+++ Client/src/components/module/Banner.js
@@ -3,7 +3,12 @@ import styles from "@/components/module/Banner.module.css"
 const Banner = () => {
     return (
         <div className={styles.banner}>
-            <Image src="/images/banner.jpg" alt="banner" width={3500} height={1600} />
+            <div className={styles.background}>
+                <Image src="/images/7.jpg" alt="banner" width={3500} height={1600} />
+            </div>
+            <div className={styles.airplan}>
+                <Image className={styles.airplan} src="/images/8.png" alt="banner" width={1000} height={800} />
+            </div>
         </div>
     )
 }
